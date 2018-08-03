4a1dd98e284c1f9e2149537951c0ad63ca15581e
izziperron <izziperron@gmail.com>
2018-08-03 15:21:17 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,40 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+ get '/' do
+   erb :'index.html'
+ end
+ 
+ get '/about.html' do
+   erb :'about.html'
+ end
+ 
+get '/facts.html' do
+  erb :'facts.html'
+end
+ 
+ get '/homepage.html' do
+   erb :'home.html'
+ end
+ 
+ get '/me_too.html' do
+   erb :'me_too.html'
+ end
+ 
+  get '/health.html' do
+   erb :'health.html'
+ end
+ 
+ get '/stereotypes.html' do
+   erb :'stereotypes.html'
+ end
+ 
+ get '/wage_gap.html' do
+   erb :'wage_gap.html'
+ end
+ 
+ get '/women.html' do
+   erb :'women.html'
+ end
+ 
 end
